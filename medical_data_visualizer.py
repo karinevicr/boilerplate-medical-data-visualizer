8a00963dd86ba15d4efa772e626a5d28a06b5d32
karinevicr <karine.paixao@sou.inteli.edu.br>
2024-09-26 20:03:33 -0300

    feat: adiciona ponderada free code camp

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('boilerplate-medical-data-visualizer/medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = ((df['weight'] / (df['height'] / 100) ** 2)).apply(lambda x: 1 if x > 25 else 0)
+
 
 # 3
+df['cholesterol'] = (df['cholesterol'].apply(lambda x: 0 if x == 1 else 1))
+df['gluc'] = (df['gluc'].apply(lambda x: 0 if x == 1 else 1))
 
 
-# 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
 
-
-    # 6
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=["active", "alco", "cholesterol", "gluc", "overweight", "smoke"])
     
 
-    # 7
+    df_cat['total'] = 1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index =False).count()
 
+    # sns.set_theme(style='darkgrid')
+    fig = sns.catplot(x = 'variable', y = 'total', data = df_cat, kind = 'bar', 
+                      col = 'cardio', hue = 'value').fig
 
-
-    # 8
-    fig = None
-
-
-    # 9
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
 def draw_heat_map():
-    # 11
-    df_heat = None
 
-    # 12
-    corr = None
+    # Filtrar o DataFrame
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) & 
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) & 
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
-    # 13
-    mask = None
+    # Calcular a matriz de correlação
+    corr = df_heat.corr(method='pearson')
 
+    # Gerar uma máscara para o triângulo superior
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+    # Criar a figura e os eixos para o mapa de calor
+    fig, ax = plt.subplots(figsize=(12, 12))
 
-    # 14
-    fig, ax = None
+    # Criar o mapa de calor
+    sns.heatmap(corr, annot=True, fmt='.1f', linewidths=1, mask=mask, square=True, center=0.8,
+                cbar_kws={'shrink': 0.5})
 
-    # 15
-
-
-
-    # 16
+    # Salvar a figura gerada
     fig.savefig('heatmap.png')
+
+    # Retornar a figura
     return fig
